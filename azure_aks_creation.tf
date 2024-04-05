6c696373a4fb3c212fe3d8661a347f4199a77fa5
Sami Islam <islam.samipkr@gmail.com>
2024-04-05 07:52:16 -0400

    Update azure_aks_creation.tf

--- azure_aks_creation.tf
+++ azure_aks_creation.tf
@@ -1,48 +1,20 @@
 #This is an Azure Montreal College Information Technology - Lab Session
 
-locals{ 
-  cluster_names=["k8batcha06","k9batcha06","k10batcha06","k11batcha06","k12batcha06"]
-}
 resource "azurerm_resource_group" "azureresourcegroup" {
   name     = "MCIT_lab_session"
   location = "Canada Central"
 }
-resource "azurerm_kubernetes_cluster" "batchabcd" {
-  for_each            = {for cluster in local.cluster_names: cluster=>cluster}
-  name                = "${var.prefix}cluster"
-  location            = azurerm_resource_group.azureresourcegroup.location
-  resource_group_name = azurerm_resource_group.azureresourcegroup.name
-  dns_prefix          = "exampleaks1"
 
-  default_node_pool {
-    name       = "default"
-    node_count = 1
-    vm_size    = "Standard_D2_v2"
-  }
-
-  identity {
-    type = "SystemAssigned"
-  }
-
+resource "azurerm_storage_account" "azurermmcitcountexample" {
+  count=5
+  name                     = "${var.prefix}-storage${count.index}"
+  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
+  location                 = azurerm_resource_group.mcit420zz5um.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  access_tier=var.access_tier
+  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
   tags = {
-    Environment = "Production"
+    environment = var.environment
   }
 }
-
-output "client_certificate" {
-  value     = [for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.kube_config.0.client_certificate]
-  sensitive = true
-}
-
-output "kube_config" {
-  value = [for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw]
-
-  sensitive = true
-}
-output "kube_id"{
-  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
-}
-output "kube_name"{
-  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
-}
-
