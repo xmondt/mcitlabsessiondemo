7dab28e45dc854136677fc414f7e20fa08fc0000
Sami Islam <islam.samipkr@gmail.com>
2024-04-05 07:54:29 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -15,3 +15,24 @@ variable "prefix"{
  type=string
  default="mcit"
 }
+variable "account_tier"{
+  type=string
+  default="Standard"
+}
+variable "account_replication_type"{
+  type=string
+  default="GRS"
+}
+variable "cross_tenant_replication_enabled"{
+ type=bool
+ default=false
+}
+variable "prefix"{
+ type=string
+ default="mcit"
+}
+
+variable "environment"{
+ type=string
+ default="staging"
+}
