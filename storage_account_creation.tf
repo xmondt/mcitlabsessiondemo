218ee76a63e3ac8c392ce0126a31b0f45d47b110
Sami Islam <islam.samipkr@gmail.com>
2024-04-05 08:01:45 -0400

    Update storage_account_creation.tf

--- storage_account_creation.tf
+++ storage_account_creation.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "azureresourcegroup" {
 resource "azurerm_storage_account" "azurermmcitcountexample" {
   count=5
   name                     = "${var.prefix}-storage${count.index}"
-  resource_group_name      = azurerm_resource_group.mcit420zz5um.name
-  location                 = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
+  location                 = azurerm_resource_group.azureresourcegroup.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   access_tier=var.access_tier
