a9ee90013b1a5f2fd55da87a65028f43f8491166
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-06 16:36:32 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -18,3 +18,13 @@ for 3rd element in -->inside the list variable montreallistofuniversities--->"va
 for 4th element in -->inside the list variable montreallistofuniversities--->"cegep" 4th element
 for 5th element in -->inside the list variable montreallistofuniversities--->"rosemontcollege" 5th element
 */
+output "montrealresturants"{
+   value=[for montrealresturant in var.montrealresturants:montrealresturant]
+}
+/*
+for 1st element in -->inside the list variable montreallistofresturants--->"holder"1st element
+for 2nd element in -->inside the list variable montreallistofresturants--->"mcdonald" 2nd element
+for 3rd element in -->inside the list variable montreallistofresturants--->"timhortons" 3rd element
+for 4th element in -->inside the list variable montreallistofresturants--->"brasserie" 4th element
+for 5th element in -->inside the list variable montreallistofresturants--->"steakhouse" 5th element
+/*
