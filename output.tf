0b6098019ac62290ebfa457678317f504d6d31d7
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-06 15:31:38 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,3 +1,9 @@
 output "summersportslists"{
    value=[for summersports in var.summersports:summersport]
 }
+/*
+for 1st element in -->inside the list variable summerlistofsports--->"basketball"1st element
+for 2nd element in -->inside the list variable summerlistofsports--->"badminton" 2nd element
+for 3rd element in -->inside the list variable summerlistofsports--->"tabletennis" 3rd element
+for 4th element in -->inside the list variable summerlistofsports--->"football" 3rd element
+*/
