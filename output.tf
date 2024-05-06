b017abd5594def68fc19c9a00b3a83e09a6e5902
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-06 16:25:52 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -8,3 +8,13 @@ for 3rd element in -->inside the list variable summerlistofsports--->"badminton"
 for 4th element in -->inside the list variable summerlistofsports--->"tabletennis" 4th element
 for 5th element in -->inside the list variable summerlistofsports--->"football" 5th element
 */
+output "montrealuniversitieslists"{
+   value=[for montrealuniversity in var.montrealuniversities:montrealuniversity]
+}
+/*
+for 1st element in -->inside the list variable montreallistofuniversities--->"mcgill"1st element
+for 2nd element in -->inside the list variable montreallistofuniversities--->"concordia" 2nd element
+for 3rd element in -->inside the list variable montreallistofuniversities--->"vanier" 3rd element
+for 4th element in -->inside the list variable montreallistofuniversities--->"cegep" 4th element
+for 5th element in -->inside the list variable montreallistofuniversities--->"rosemontcollege" 5th element
+*/
