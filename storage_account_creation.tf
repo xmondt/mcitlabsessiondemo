d696bf438b62bd223cdac73a6e845fd978b48ed0
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-08 17:58:14 -0400

    Update storage_account_creation.tf

--- storage_account_creation.tf
+++ storage_account_creation.tf
@@ -1,1 +1,23 @@
+STORAGE TF
+
+#This is an Azure Montreal College Information Technology - Lab Session
+
+resource "azurerm_resource_group" "azureresourcegroup" {
+  name     = "MCIT_lab_session"
+  location = "Canada Central"
+}
+
+resource "azurerm_storage_account" "azurermmcitcountexample" {
+  count=5
+  name                     = "${var.prefix}storage${count.index}"
+  resource_group_name      = azurerm_resource_group.azureresourcegroup.name
+  location                 = azurerm_resource_group.azureresourcegroup.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
+  access_tier=var.access_tier
+  cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
+  tags = {
+    environment = var.environment
+  }
+}
 
