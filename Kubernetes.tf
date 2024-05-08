2860f44a35bd470978ac75d268757ddbeb53d508
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-08 18:11:37 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -1,7 +1,7 @@
 locals{ 
   cluster_names=["mixer","producer","buyer","seller","merchant"]
 }
-resource "azurerm_resource_group" "azureresourcegroup" {
+resource "azurerm_resource_group" "azureresourcegroup1" {
   name     = "Music_resource_group"
   location = "Canada Central"
 }
