5a6ec6484cc87d427e40871a6d0a095130ca156c
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-10 00:31:47 -0400

    Update function2.tf

--- function2.tf
+++ function2.tf
@@ -1,1 +1,13 @@
+variable "original_string"{
+ default = "Samuel Dimbola"
+}
 
+# Function 1a: Uppercase the string
+output "uppercase_string" {
+ value = upper(var.original_string)
+}
+
+# Function 2a: Lowercase the string
+output "lowercase_string" {
+ value = lower(var.original_string)
+}
