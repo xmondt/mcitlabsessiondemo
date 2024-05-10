679f8a9a0898eb96088c5d5a0b8b61902c5662e9
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-10 00:40:23 -0400

    Update function2.tf

--- function2.tf
+++ function2.tf
@@ -11,3 +11,13 @@ output "uppercase_string1" {
 output "lowercase_string1" {
  value = lower(var.original_string1)
 }
+
+# Function 4: Replace "Hello" with "Hi"
+output "replaced_string1" {
+ value = replace(var.original_string1, "Samuel", "Dimbola")
+}
+
+# Function 4: Replace "Hi" with "Hello"
+output "replaced_string1" {
+ value = replace(var.original_string1, "Dimbola", "Samuel")
+}
