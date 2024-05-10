488de66f061804b1abda7c18f748ea8072c17f6c
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-09 21:36:50 -0400

    Update function.tf

--- function.tf
+++ function.tf
@@ -2,7 +2,6 @@ variable "original_string"{
  default = "Montreal College Information Technology"
 }
 
-
 # Function 1: Length of the string
 output "string_length"{
  value = length(var.original_string)
