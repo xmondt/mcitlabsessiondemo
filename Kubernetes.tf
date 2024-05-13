8c142af3fc7f795aeeb8df6eac169c384c536e02
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-13 15:51:48 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -1,7 +1,7 @@
 locals{ 
   cluster_names=["mixer","producer","buyer","seller","merchant"]
 }
-resource "azurerm_resource_group" "azureresourcegroup" {
+resource "azurerm_resource_group" "myazureresourcegroup" {
   name     = "MCIT_resource_group"
   location = "Canada Central"
 }
