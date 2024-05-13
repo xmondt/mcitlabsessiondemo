01cd8cccd26e5851eb2a62ac818f3d2d7f44045c
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-13 17:29:12 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -9,6 +9,7 @@ resource "azurerm_resource_group" "musicresourcegroup" {
 resource "azurerm_resource_group" "concertresourcegroup" {
   name     = "CONCERT_resource_group"
   location = "UAE Central"
+}
 
 output "company_name_output"{
     value=var.company_name
