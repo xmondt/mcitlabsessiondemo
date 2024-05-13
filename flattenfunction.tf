66a06b6bfa822c8fcd05a6236f71495ddd83fb8e
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-12 23:03:52 -0400

    Update flattenfunction.tf

--- flattenfunction.tf
+++ flattenfunction.tf
@@ -1,1 +1,12 @@
+variable "simple_nested_list" {
+ type = list(list(string))
+ default = [["a","b"], ["c","d"], ["e","f"]]
+}
 
+locals {
+  flattened_list = flatten(var.simple_nested_list)
+}
+
+output "flattened_list_output" {
+  value = local.flattened_list
+}
