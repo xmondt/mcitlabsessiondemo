7eeacbbfaaebac505c1b5587d4a644285a5a88d0
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-12 23:25:05 -0400

    Update mapfunction.tf

--- mapfunction.tf
+++ mapfunction.tf
@@ -1,1 +1,9 @@
+variable "nested_map" {
+  type    = map(map(string))
+  default = {
+    group1 = {"name" = "Alice", "age" = "25"}
+    group2 = {"name" = "Bob", "age" = "30"}
+  }
 
+locals {
+  flattened_map = flatten([for k, v in var.nested_map : [for kk, vv in v : {group = k, key = kk, value = vv}]])
