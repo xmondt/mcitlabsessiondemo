5da08dfc9eec8d0abe1b078611817855f5069bf2
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-14 17:36:47 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -35,9 +35,8 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
   tags = {
     Environment = "Production"
   }
-
 }
-
+/*
 output "client_certificate" {
   value     = [for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.kube_config.0.client_certificate]
   sensitive = true
@@ -48,3 +47,10 @@ output "kube_config" {
 
   sensitive = true
 }
+output "kube_id"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
+}
+output "kube_name"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.name ]
+}
+*/
