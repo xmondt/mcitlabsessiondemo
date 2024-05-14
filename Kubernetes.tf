8f58d4d1bf37da852d273b6e2ad0d58fd9b3d03f
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-14 17:15:14 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -15,3 +15,23 @@ output "company_name_output"{
     value=var.company_name
 }
 
+resource "azurerm_kubernetes_cluster" "batchabcd" {
+  for_each            = {for cluster in var.classworkclusters: cluster=>cluster}
+  name                = "${var.prefix}${each.key}"
+  location            = azurerm_resource_group.azureresourcegroup.location
+  resource_group_name = azurerm_resource_group.azureresourcegroup.name
+  dns_prefix          = var.dnsprefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
