743cbb41fefd66a3b40b5189e1b0b92637186153
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-15 01:34:27 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -17,8 +17,9 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
 default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_D2_v2"
+    vm_size    = "Standard_Ds2_v2"
   }
+}
 
   identity {
     type = "SystemAssigned"
