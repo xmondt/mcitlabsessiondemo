75bb16545a512be2244baae87cc759f16067010c
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-15 01:23:33 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -14,6 +14,17 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
   dns_prefix          = "musicdns-${each.key}"
 }
 
+default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
+
 output "kubernetes_cluster_names" {
   value = [for cluster in azurerm_kubernetes_cluster.batchabcd : cluster.name]
 }
