6427dea30930b002505f2ae83f2968dcf577a577
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-15 01:32:00 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -17,7 +17,7 @@ resource "azurerm_kubernetes_cluster" "batchabcd" {
 default_node_pool {
     name       = "default"
     node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
@@ -25,9 +25,31 @@ default_node_pool {
   }
 }
 
+tags = {
+    Environment = "Production"
+  }
+}
+/*
+output "client_certificate" {
+  value     = [for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.kube_config.0.client_certificate]
+  sensitive = true
+}
+
+output "kube_config" {
+  value = [for cluster in azurerm_kubernetes_cluster.batchabcd: cluster.kube_config_raw]
+
+  sensitive = true
+}
+output "kube_id"{
+  value=[for cluster in azurerm_kubernetes_cluster.batchabcd:cluster.id ]
+}
+
 output "kubernetes_cluster_names" {
   value = [for cluster in azurerm_kubernetes_cluster.batchabcd : cluster.name]
 }
+*/
+
+
 
 resource "azurerm_resource_group" "concertresourcegroup" {
   name     = "CONCERT_resource_group"
