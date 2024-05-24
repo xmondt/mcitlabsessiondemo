4377c34ef6697188b9d77feeaeeacbab3648adcd
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-24 16:49:35 -0400

    Update examquestion12.tf

--- examquestion12.tf
+++ examquestion12.tf
@@ -6,44 +6,44 @@ provider "azurerm" {
   tenant_id      = "your_tenant_id"
 }
 
-resource "azurerm_resource_group" "example" {
-  name     = "video2resourcegroup"
+resource "azurerm_resource_group" "video2resourcegroup" {
+  name     = "VIDEO2_resource_group"
   location = "Canada East"
 }
 
-resource "azurerm_virtual_network" "example" {
+resource "azurerm_virtual_network" "videovn" {
   name                = "videovn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.video2resourcegroup.location
+  resource_group_name = azurerm_resource_group.video2resourcegroup.name
 }
 
-resource "azurerm_virtual_machine" "example" {
+resource "azurerm_virtual_machine" "videovm" {
   count = 5
   name                  = element(["sixthvm", "seventhvm", "eighthvm", "ninthvm", "tenthvm"], count.index)
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.video2resourcegroup.name
+  location            = azurerm_resource_group.video2resourcegroup.location
   vm_size               = "Standard_DS2_v2"
-  network_interface_ids = [azurerm_network_interface.example[count.index].id]
+  network_interface_ids = [azurerm_network_interface.videovn[each.key].id]
 }
 
-resource "azurerm_network_interface" "example" {
+resource "azurerm_network_interface" "videovn" {
   count = 5
   name                = "videovn-nic-${count.index}"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.video2resourcegroup.location
+  resource_group_name = azurerm_resource_group.video2resourcegroup.name
 
   ip_configuration {
-    name                          = "ipconfig-${count.index}"
-    subnet_id                     = (link unavailable)
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.videovn.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "videovn" {
   name                 = "videovn-subnet"
-  resource_group_name = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name = azurerm_resource_group.video2resourcegroup.name
+  virtual_network_name = azurerm_virtual_network.video2resourcegroup.name
   address_prefixes     = ["10.0.1.0/24"]
 }
 
