ad243bf8842969d82084d9ff0b713690c3b870d2
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-24 02:04:56 -0400

    Update myexam.tf

--- myexam.tf
+++ myexam.tf
@@ -35,8 +35,9 @@ resource "azurerm_network_interface" "musicvn" {
 }
 
 resource "azurerm_virtual_machine" "musicvn" {
-  for_each            = {for vm in local.vm_list: vm=>vm}
-  name                  = "${var.prefix}-vm"
+  count        = 5
+  for_each     = {for vm in local.vm_list: vm=>vm}
+  name         = "${var.prefix}-vm"
   location              = azurerm_resource_group.musicresourcegroup.location
   resource_group_name   = azurerm_resource_group.musicresourcegroup.name
   network_interface_ids = [azurerm_network_interface.musicvn.id]
