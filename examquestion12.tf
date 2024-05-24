34ba6ee6a79128291231a2fdc7e6d8124050f55c
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-24 16:31:19 -0400

    Update examquestion12.tf

--- examquestion12.tf
+++ examquestion12.tf
@@ -1,55 +1,61 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "latest"
-    }
-  }
-}
-
 provider "azurerm" {
   version = "latest"
-  features {}
+  subscription_id = "your_subscription_id"
+  client_id      = "your_client_id"
+  client_secret = "your_client_secret"
+  tenant_id      = "your_tenant_id"
 }
 
-resource "azurerm_resource_group" "rg" {
+resource "azurerm_resource_group" "example" {
   name     = "video2resourcegroup"
   location = "Canada East"
 }
 
-resource "azurerm_virtual_network" "vn" {
+resource "azurerm_virtual_network" "example" {
   name                = "videovn"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_virtual_machine" "vm" {
-  count = length(var.vms)
+resource "azurerm_virtual_machine" "example" {
+  count = 5
+  name                  = element(["sixthvm", "seventhvm", "eighthvm", "ninthvm", "tenthvm"], count.index)
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  vm_size               = "Standard_DS2_v2"
+  network_interface_ids = [azurerm_network_interface.example[count.index].id]
+}
 
-  name                  = var.vms[count.index].name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  vm_size               = var.vms[count.index].vm_size
-  image {
-    id = var.vms[count.index].image
+resource "azurerm_network_interface" "example" {
+  count = 5
+  name                = "videovn-nic-${count.index}"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "ipconfig-${count.index}"
+    subnet_id                     = (link unavailable)
+    private_ip_address_allocation = "Dynamic"
   }
-  admin_username = var.vms[count.index].admin_username
-  admin_password = var.vms[count.index].admin_password
-  virtual_network_ids = [azurerm_virtual_network.vn.id]
 }
 
-variable "vms" {
-  type = list(object({
-    name            = string
-    resource_group = string
-    location       = string
-    vm_size        = string
-    image          = string
-    admin_username = string
-    admin_password = string
-    virtual_network = string
-  }))
+resource "azurerm_subnet" "example" {
+  name                 = "videovn-subnet"
+  resource_group_name = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example.name
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
-
+# File: virtual_machines.yml
+virtual_machines:
+  - name: sixthvm
+    vm_size: Standard_DS2_v2
+  - name: seventhvm
+    vm_size: Standard_DS2_v2
+  - name: eighthvm
+    vm_size: Standard_DS2_v2
+  - name: ninthvm
+    vm_size: Standard_DS2_v2
+  - name: tenthvm
+    vm_size: Standard_DS2_v2
