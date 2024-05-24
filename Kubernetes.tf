fc7531246e092e018ba74eeb4fe880ad81267a66
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-24 10:45:53 -0400

    Update Kubernetes.tf

--- Kubernetes.tf
+++ Kubernetes.tf
@@ -2,16 +2,16 @@ locals{
   cluster_names = ["mixer", "producer", "buyer", "seller", "merchant"]
 }
 
-resource "azurerm_resource_group" "musicresourcegroup" {
-  name     = "MUSIC_resource_group"
+resource "azurerm_resource_group" "musicresourcegroup1" {
+  name     = "MUSIC_resource_group1"
   location = "Canada Central"
 }
 
 resource "azurerm_kubernetes_cluster" "batchabcd" {
   for_each = { for cluster in local.cluster_names : cluster => cluster }
   name                = "MUSIC_${each.key}"
-  location            = azurerm_resource_group.musicresourcegroup.location
-  resource_group_name = azurerm_resource_group.musicresourcegroup.name
+  location            = azurerm_resource_group.musicresourcegroup1.location
+  resource_group_name = azurerm_resource_group.musicresourcegroup1.name
   dns_prefix          = "musicdns-${each.key}"
 
   identity{
