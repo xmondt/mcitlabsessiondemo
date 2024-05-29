e0efa5ebafbeaac0afaa26fda2620d5a57788dfa
xmondt <168039714+xmondt@users.noreply.github.com>
2024-05-29 19:29:28 -0400

    Update mine12.tf

--- mine12.tf
+++ mine12.tf
@@ -1,9 +1,3 @@
-provider "azurerm" {
-  subscription_id = "your_subscription_id"
-  client_id      = "your_client_id"
-  client_secret = "your_client_secret"
-  tenant_id      = "your_tenant_id"
-}
 
 resource "azurerm_resource_group" "video2resourcegroup" {
   name     = "VIDEO2_resource_group"
