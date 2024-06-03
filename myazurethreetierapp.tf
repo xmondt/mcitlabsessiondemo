e63f00042c27891216f43144124d5af1018fb911
xmondt <168039714+xmondt@users.noreply.github.com>
2024-06-03 16:49:12 -0400

    Create myazurethreetierapp.tf

---
+++ myazurethreetierapp.tf
@@ -0,0 +1,226 @@
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "Mcitterraformrg"
+  location = "Canada Central"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnets
+resource "azurerm_subnet" "web" {
+  name                 = "web-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "app" {
+  name                 = "app-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "db" {
+  name                 = "db-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+# Network Security Groups
+resource "azurerm_network_security_group" "web_nsg" {
+  name                = "web-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "app_nsg" {
+  name                = "app-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_group" "db_nsg" {
+  name                = "db-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Network Security Rules
+resource "azurerm_network_security_rule" "web_allow_http" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = "allow-http"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.web_nsg.name
+}
+
+resource "azurerm_network_security_rule" "web_allow_https" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = "allow-https"
+  priority                    = 101
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  network_security_group_name = azurerm_network_security_group.web_nsg.name
+}
+
+resource "azurerm_network_security_rule" "app_allow_inbound" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = "allow-inbound"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "8080"
+  source_address_prefix       = "10.0.1.0/24"
+  destination_address_prefix  = "10.0.2.0/24"
+  network_security_group_name = azurerm_network_security_group.app_nsg.name
+}
+
+resource "azurerm_network_security_rule" "db_allow_inbound" {
+  resource_group_name = azurerm_resource_group.rg.name
+  name                        = "allow-inbound-db"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "3306"
+  source_address_prefix       = "10.0.2.0/24"
+  destination_address_prefix  = "10.0.3.0/24"
+  network_security_group_name = azurerm_network_security_group.db_nsg.name
+}
+
+# Subnet Associations with NSGs
+resource "azurerm_subnet_network_security_group_association" "web_assoc" {
+  subnet_id                 = azurerm_subnet.web.id
+  network_security_group_id = azurerm_network_security_group.web_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "app_assoc" {
+  subnet_id                 = azurerm_subnet.app.id
+  network_security_group_id = azurerm_network_security_group.app_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "db_assoc" {
+  subnet_id                 = azurerm_subnet.db.id
+  network_security_group_id = azurerm_network_security_group.db_nsg.id
+}
+
+# Web Tier Virtual Machine
+resource "azurerm_linux_virtual_machine" "web_vm" {
+  name                = "webVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.administrator_login
+  admin_password      = var.administrator_login_password
+  network_interface_ids = [
+    azurerm_network_interface.web_nic.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_interface" "web_nic" {
+  name                = "webNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.web.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# App Tier Virtual Machine
+resource "azurerm_linux_virtual_machine" "app_vm" {
+  name                = "appVM"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_B1s"
+  admin_username      = var.administrator_login
+  admin_password      = var.administrator_login_password
+  network_interface_ids = [
+    azurerm_network_interface.app_nic.id,
+  ]
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_network_interface" "app_nic" {
+  name                = "appNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.app.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Database Tier - Azure Database for MySQL
+resource "azurerm_mysql_server" "mysql" {
+  name                = "mysqlserver"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  administrator_login = var.db_admin_username
+  administrator_login_password = var.db_admin_password
+  sku_name            = "GP_Gen5_2"
+  storage_mb          = 5120
+  version             = "5.7"
+  auto_grow_enabled   = true
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+  public_network_access_enabled = false
+  ssl_enforcement_enabled       = true
+
+}
+
+resource "azurerm_mysql_database" "exampledb" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mysql_server.mysql.name
+  charset             = "utf8"
+  collation           = "utf8_general_ci"
+}
+
+# Output the Public IP of the Web VM
+output "web_vm_public_ip" {
+  value = azurerm_linux_virtual_machine.web_vm.public_ip_address
+}
